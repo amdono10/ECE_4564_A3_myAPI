c48200f4c1c7533ca70f313bd40a3eebb848f396
Brendan O'Connell <obren800@vt.edu>
2017-11-30 18:01:28 -0500

    upload gets with canvas access token

---
+++ getData.py
@@ -0,0 +1,12 @@
+import json
+import requests
+
+
+url = 'https://canvas.vt.edu/api/v1/courses/57754/assignments'
+header = {"Authorization": "Bearer 4511~hNgxEqiPIYBBsV6jwwogaa1Jit4cBsPhFKOzPFjRpzws7qhhZd2PP5MUlt2uPhLI"}
+           
+r = requests.get(url, headers=header)
+
+for item in r.json():
+    print (item["name"])
+    print (item["points_possible"])
